9e6baf7ccd28a94bab6e104130e70eba2c916634
@Vika@ <frantsuhvika@gmail.com>
2018-06-13 23:07:23 +0300

    public game

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MemoryCard : MonoBehaviour
+{
+	[SerializeField]
+	private SceneController _controller;
+	private int _id;
+
+    
+	public int id()
+	{
+		return _id;
+	}
+    
+	public void SetCard(int id, Sprite image)
+	{
+		_id = id;
+		GetComponent<SpriteRenderer>().sprite = image;
+	}
+}
